ff832e996dc74f61b06f31dbccecec0445177637
Renato <renatopkim@htomail.com>
2022-05-27 21:27:59 -0300

    exclusão de arqruivos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <h1>teste</h1>
-        </p>
-      </header>
+    <div>
+      <h1>My App</h1>
+      <h1>React</h1>
     </div>
   );
 }
