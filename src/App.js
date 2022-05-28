e2d02ae84f5d4d31be9ed64026bc2ae09877d34e
Renato <renatopkim@htomail.com>
2022-05-27 22:50:49 -0300

    props e component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
+const BemVindo = (props) => {
+  return (
+    <div>
+      <h1>Bem Vindo</h1>
+      <h1>{ props.nome }</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>My App</h1>
+      <BemVindo nome="Renato" />
+      <BemVindo nome="Pereira" />
+      <BemVindo nome="da Cruz" />
       <h1>React</h1>
     </div>
   );
