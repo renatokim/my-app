ad6feb1241dd4a4adafe707a515d51dd2d9e1209
Renato <renatopkim@htomail.com>
2022-05-27 23:56:24 -0300

    Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+const Sobre = (props) => {
+  return (
+    <div>
+      <h2>Sobre: { props.sobre.nome } </h2>
+      <h2>Sobre: { props.sobre.sobrenome } </h2>
+    </div>
+  );
+}
+
 const BemVindo = (props) => {
   return (
     <div>
-      <h1>Bem Vindo</h1>
-      <h1>{ props.nome }</h1>
+      <h2>Nome: { props.nome } {props.sobrenome}</h2>
+      <Sobre sobre={props} />
     </div>
   );
 }
@@ -12,11 +21,11 @@ const BemVindo = (props) => {
 function App() {
   return (
     <div>
-      <h1>My App</h1>
-      <BemVindo nome="Renato" />
-      <BemVindo nome="Pereira" />
-      <BemVindo nome="da Cruz" />
-      <h1>React</h1>
+      <h1>Bem Vindo!</h1>
+      <BemVindo nome="Renato" sobrenome="Cruz" />
+      <BemVindo nome="Fabianne" sobrenome="Pousada" />
+      <BemVindo nome="Isabella" sobrenome="Pereira" />
+      <h1>Curso React</h1>
     </div>
   );
 }
